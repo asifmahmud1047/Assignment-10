245db6f26fd20408d5e702e59f2cabb32bbfd514
asifmahmud1047 <asif.mahmud1047@gmail.com>
2025-04-29 22:36:31 +0600

    updated

---
+++ src/Components/Homelayout/Homelayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../Page/Banner/Banner";
+
+const Homelayout = () => {
+  return (
+    <div className="w-11/12 mx-auto my-32">
+      <Banner></Banner>
+    </div>
+  );
+};
+
+export default Homelayout;
