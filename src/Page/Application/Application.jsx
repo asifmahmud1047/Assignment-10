be1b79bf347903e8c132f896381bfd41d3a04acd
asifmahmud1047 <asif.mahmud1047@gmail.com>
2025-05-06 19:48:23 +0600

    search function added

--- src/Page/Application/Application.jsx
+++ src/Page/Application/Application.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useLoaderData } from "react-router-dom";
  import { toast } from "react-toastify";
  import "react-toastify/dist/ReactToastify.css";
@@ -6,8 +6,29 @@ import { useLoaderData } from "react-router-dom";
 const Application = () => {
   const applications = useLoaderData();
 
-  const [card, setCard] = useState(applications);
+  const [card, setCard] = useState(applications); //main visa data
+  const [search, setSearch] = useState(""); // search term
 
+  // data filter according to search term
+  useEffect(() => {
+    if (search.trim() === "") {
+      setCard(applications); // if search filter empty, show previous data
+    } else {
+      fetch(
+        `https://assignment-10-server-two-sand.vercel.app/apply?searchPrams=${search}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setCard(data); // search result set in the state
+        })
+        .catch((error) => {
+          console.error("Error fetching search data:", error);
+          toast.error("Failed to fetch search results.");
+        });
+    }
+  }, [search, applications]); // effect will run if search data change
+
+  // function of visa cancellation
   const handleCancel = (id) => {
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this visa?"
@@ -20,36 +41,31 @@ const Application = () => {
         .then((data) => {
           if (data.deletedCount > 0) {
             toast.success("Visa deleted successfully!");
-            setCard(card.filter((v) => v._id !== id));
+            setCard(card.filter((v) => v._id !== id)); // ডিলিট হওয়া ভিসা ফিল্টার করা।
           }
         });
     }
   };
-  // const handleCancel = (id) => {
-  //     fetch(`https://assignment-10-server-two-sand.vercel.app/apply/${id}`,{
-  //         method:'DELETE'
-  //  })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //         if(data.deletedCount>0){
-  //             const remainingCard = card.filter(card => card._id !== id)
-  //             setCard(remainingCard)
-  //             return toast.success('Deleted Successfull')
-  //         }
-  //     })
-  //     .catch((error) => {
-  //         console.error("Error deleting application:", error);
-  //         toast.error("Failed to delete the application.");
-  //     });
-  // }
 
   return (
     <div className="my-20 w-10/12 mx-auto">
       <h1 className="text-3xl font-bold text-center mb-10">
         My Visa Applications
       </h1>
+
+      {/* search input */}
+      <div className="mb-9">
+        <input
+          type="text"
+          placeholder="Search Your Country Name"
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+      </div>
+
+      {/** show filtered data */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {applications.map((app) => (
+        {card.map((app) => (
           <div key={app._id} className="bg-white shadow-md rounded-lg p-6">
             <div className="mb-4">
               <img
